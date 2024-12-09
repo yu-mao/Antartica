18c5d9a169536e5331bd8e9f3adb5581e876888d
Jiayao Yu <yjybxx2@yahoo.com>
2024-12-09 10:32:29 +0100

    Added mountain gate openning animation in main scene.

--- Assets/Scripts/MountainGate.cs
+++ Assets/Scripts/MountainGate.cs
@@ -1,28 +1,17 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Android;
 
 public class MountainGate : MonoBehaviour
 {
-    public bool isToOpenGate = false;
     [SerializeField] private Animator _animator;
 
 
     private void OnTriggerEnter(Collider other)
     {
-        // TODO: check if it's XR rig 
-        // TODO: test animation
-        OpenMountainGate();
-        // TODO: loan WhereIsHammer scene
-    }
-
-    private void OpenMountainGate()
-    {
-        if (isToOpenGate)
+        if (other.CompareTag("Player"))
         {
-            _animator.SetBool("isToOpenGate", true);
+            _animator.SetBool("isToOpenGate", true); // open mountain gate
         }
+        
+        // TODO: loan WhereIsHammer scene
     }
 }
